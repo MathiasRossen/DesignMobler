b4ab9e10068bf44af3140ab31860d1268494700b
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-10-27 11:34:54 +0200

    Can test

---
+++ DesignMobler/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignMobler
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void CanTest()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
