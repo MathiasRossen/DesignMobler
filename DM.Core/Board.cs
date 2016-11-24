2bfe1c7fdb273f9f345f48de4f5757ab80519eaa
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-11-24 12:43:30 +0100

    Added comment to board

--- DM.Core/Board.cs
+++ DM.Core/Board.cs
@@ -60,3 +60,12 @@
         #endregion
     }
 }
+// Mål:
+//          Form 1/2 + udtræk til fast,2,3 plader - 1,2,3 plader 
+//          Ved fast plade er pladerne i ét stykke og ikke 2x halv.
+//    
+//  Form1   Kvadrat: 90x90, 100x100, 110x110, 120x120
+//  Form2 Rektangel: Bredde x Længde
+//           Bredde: 90,100,110,120
+//           Længde: 140,160,180,200,220,240,250,260,270
+// 
